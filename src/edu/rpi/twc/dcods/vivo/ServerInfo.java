8beb56f6406b6fea2cf8d06d0c5cd2c6d429955d
Patrick West <pwest@dco.tw.rpi.edu>
2016-03-30 10:51:21 -0600

    Added comments for the different config params

--- src/edu/rpi/twc/dcods/vivo/ServerInfo.java
+++ src/edu/rpi/twc/dcods/vivo/ServerInfo.java
@@ -40,59 +40,71 @@ public class ServerInfo {
         return "" ;
 	}
 
+    /* vivo root email */
 	public String getRootName( ServletContext ctx )
 	{
 		return getProperty( this.rootName, ctx ) ;
 	}
 	
+    /* vivo root password */
 	public String getRootPassword( ServletContext ctx )
 	{
 		return getProperty( this.rootPassword, ctx ) ;
 	}
 	
+    /* DCO Handle Service URL */
 	public String getHandleURL( ServletContext ctx )
 	{
 		return getProperty( this.handleURL, ctx ) ;
 	}
 	
+    /* DCO CKAN base URL. The API URL is appended to this */
 	public String getCkanURL( ServletContext ctx ){
 		return getProperty( this.ckanURL, ctx ) ;
 	}
 	
+    /* DCO CKAN API Key used to create and modify packages */
 	public String getCkanApiKey( ServletContext ctx ){
 		return getProperty( this.ckanApiKey, ctx ) ;
 	}
 	
+    /* DCO schema URI */
 	public String getDCOURI( ServletContext ctx )
 	{
 		return getProperty( this.dcoURI, ctx ) ;
 	}
 	
+    /* DCO instance namespace */
 	public String getDefaultNamespace( ServletContext ctx )
 	{
 		return getProperty( this.defaultNamespace, ctx ) ;
 	}
 
+    /* in some cases want to replace the default namespace with this namespace when going to profile pages */
 	public String getBaseNamespace( ServletContext ctx )
 	{
 		return getProperty( this.baseNamespace, ctx ) ;
 	}
 
+    /* Base URL for VIVO */
 	public String getBaseURL( ServletContext ctx )
 	{
 		return getProperty( this.baseURL, ctx ) ;
 	}
 
+    /* Fuseki endpoint */
 	public String getEndpoint( ServletContext ctx )
 	{
 		return getProperty( this.endpoint, ctx ) ;
 	}
 
+    /* VIVO SPARQL update URL */
 	public String getSparqlUpdateAPI( ServletContext ctx )
 	{
 		return getProperty( this.sparqlEndpointAPI, ctx ) ;
 	}
 
+    /* VIVO SPARQL query URL */
 	public String getSparqlQueryAPI( ServletContext ctx )
 	{
 		return getProperty( this.sparqlQueryAPI, ctx ) ;
