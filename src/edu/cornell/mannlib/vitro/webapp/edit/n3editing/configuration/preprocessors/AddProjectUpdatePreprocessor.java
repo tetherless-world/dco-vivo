13e7dfff1ad6d9de8bf5eb35725a2b8da3480c42
zednis <lokipatera@gmail.com>
2016-06-03 20:23:47 -0600

    Refactored AddProjectUpdatePreprocessor and added logging statements

--- src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/preprocessors/AddProjectUpdatePreprocessor.java
+++ src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/preprocessors/AddProjectUpdatePreprocessor.java
@@ -1,64 +1,47 @@
 package edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.preprocessors;
 
-import java.util.*;
-
-import javax.servlet.http.HttpServletRequest;
-
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.ResIterator;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.Statement;
-
-import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
-import edu.cornell.mannlib.vitro.webapp.dao.VitroVocabulary;
-
 import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
 import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.BaseEditSubmissionPreprocessorVTwo;
 import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.EditConfigurationVTwo;
 import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.MultiValueEditSubmission;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 public class AddProjectUpdatePreprocessor extends BaseEditSubmissionPreprocessorVTwo {
 
+    private static final Log log = LogFactory.getLog(AddProjectUpdatePreprocessor.class);
+
     public AddProjectUpdatePreprocessor(EditConfigurationVTwo editConfig) {
         super(editConfig);
-
     }
 
 /* FIXME: This doesn't seem to be called
 */
     @Override
     public void preprocess(MultiValueEditSubmission inputSubmission, VitroRequest vreq) {
-        if(inputSubmission.hasUriValue("publicationUri")) {
-            Map<String, List<String>> urisFromForm = inputSubmission.getUrisFromForm();
-            List<String> pubUris = urisFromForm.get("publicationUri");
-            List<String> newPubUris = new ArrayList<String>();
-            for (String pubUri : pubUris) {
-                if (pubUri != null && pubUri.length() != 0 && !pubUri.equals(">SUBMITTED VALUE WAS BLANK<")) {
-                    newPubUris.add(pubUri);
-                }
-            }
-            urisFromForm.put("publicationUri", newPubUris);
-            inputSubmission.setUrisFromForm(urisFromForm);
-        }
-
-        if(inputSubmission.hasUriValue("instrumentUri")) {
-            Map<String, List<String>> urisFromForm = inputSubmission.getUrisFromForm();
-            List<String> insUris = urisFromForm.get("instrumentUri");
-            List<String> newInsUris = new ArrayList<String>();
-            for (String insUri : insUris) {
-                if (insUri != null && insUri.length() != 0 && !insUri.equals(">SUBMITTED VALUE WAS BLANK<")) {
-                    newInsUris.add(insUri);
-                }
-            }
-            urisFromForm.put("instrumentUri", newInsUris);
-            inputSubmission.setUrisFromForm(urisFromForm);
-        }
-
-//        System.out.println("Input submission: " + inputSubmission.toString());
+        log.debug("in AddProjectUpdatePreprocessor::preprocess");
+        addURIsFromForm(inputSubmission, "publicationUri");
+        addURIsFromForm(inputSubmission, "instrumentUri");
+        log.debug("Input submission: " + inputSubmission.toString());
     }
 
+    // Q: Why are we getting URIs from the form and then setting them in the form?  What are we actually doing here?
+    private void addURIsFromForm(MultiValueEditSubmission inputSubmission, String uriVariable) {
+        if (inputSubmission.hasUriValue(uriVariable)) {
+            Map<String, List<String>> urisFromForm = inputSubmission.getUrisFromForm();
+            List<String> newURIs = new ArrayList<>();
+            for (String uri : urisFromForm.get(uriVariable)) {
+                if (uri != null && uri.length() != 0 && !uri.equals(">SUBMITTED VALUE WAS BLANK<")) {
+                    newURIs.add(uri);
+                }
+            }
+            urisFromForm.put(uriVariable, newURIs);
+            inputSubmission.setUrisFromForm(urisFromForm);
+        }
+    }
 }
